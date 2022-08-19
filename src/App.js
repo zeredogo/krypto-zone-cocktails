50eccd2dcd0196b25a1466d9bf606a10c913a64f
zeredogo <zeredogo@gmail.com>
2022-08-19 01:12:29 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import pages
+import Home from './pages/Home'
+import About from './pages/About'
+import SingleCocktail from './pages/SingleCocktail'
+import Error from './pages/Error'
+// import components
+import Navbar from './components/Navbar'
+function App() {
+  return (
+    <div>
+      <h2>app component</h2>
+    </div>
+  )
+}
+
+export default App
