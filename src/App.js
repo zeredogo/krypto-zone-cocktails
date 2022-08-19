f712f6ff83493265b2770f3642b68cb196dc2305
zeredogo <zeredogo@gmail.com>
2022-08-20 00:37:21 +0100

    updating commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import pages
-import Home from './pages/Home'
-import About from './pages/About'
-import SingleCocktail from './pages/SingleCocktail'
-import Error from './pages/Error'
+import Home from './pages/Home';
+import About from './pages/About';
+import SingleCocktail from './pages/SingleCocktail';
+import Error from './pages/Error';
 // import components
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
-    </div>
-  )
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route exact path='/cocktail/:id'>
+          <SingleCocktail />
+        </Route>
+        <Route exact path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
 }
 
 export default App
