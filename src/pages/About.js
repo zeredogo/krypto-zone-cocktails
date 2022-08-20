26483602974f93772399cca9a0516eba923c4175
zeredogo <zeredogo@gmail.com>
2022-08-21 00:06:52 +0100

    updating commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,6 +4,17 @@ const About = () => {
   return (
     <section className='section about-section'>
       <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipissicing elit.
+        Eos doloribus
+        animi pariatur dignissimos nulla iure dolorum et fugit
+        minus hie.
+        Adipisci sint enim, sit quo impedit doloribus
+        accusantium, ipsum esse
+        distinctio accusamus laborum illum similique cupiditate
+        consequatur
+        quasi ab. Iure.
+      </p>
     </section>
   )
 }
