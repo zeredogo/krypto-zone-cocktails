94442f3df5b9f9a85c678fb9cffff2432b8c3762
zeredogo <zeredogo@gmail.com>
2022-08-20 23:59:19 +0100

    updating commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+    </section>
   )
 }
 
