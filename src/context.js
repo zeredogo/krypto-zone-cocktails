3003351f4492672972cb74711d830879e29587a6
zeredogo <zeredogo@gmail.com>
2022-08-24 00:22:39 +0100

    updating commit

--- src/context.js
+++ src/context.js
@@ -5,13 +5,23 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  const [loading, setLoading] = useState(true)
-  const [searchTerm, setSearchTerm] = useState('a')
-  const [cocktails, setCocktails] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('a');
+  const [cocktails, setCocktails] = useState([]);
+
+  const fetchDrinks = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return <AppContext.Provider 
   value={{
     loading,
-    searchTerm, 
     cocktails,
     setSearchTerm,
   }}
