169522019268bcf2c2b13f5914709168987aa7fb
zeredogo <zeredogo@gmail.com>
2022-08-23 23:59:38 +0100

    updating commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import CocktailList from '../components/CocktailList'
-import SearchForm from '../components/SearchForm'
+import React from 'react';
+import CocktailList from '../components/CocktailList';
+import SearchForm from '../components/SearchForm';
 
 const Home = () => {
   return (
