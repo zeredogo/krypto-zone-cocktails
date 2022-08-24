6f51f1bbe77ea7896de483b6366f141abbd5980d
zeredogo <zeredogo@gmail.com>
2022-08-25 00:01:24 +0100

    updating commit

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -4,10 +4,77 @@ import { useParams, Link } from 'react-router-dom'
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const SingleCocktail = () => {
+  const {id} = useParams();
+  const [loading, setLoading] = React.useState(false);
+  const [cocktail, setCocktail] = React.useState(null);
+
+  React.useEffect(() => {
+    setLoading(true);
+    async function getCocktail(){
+      try {
+        const response = await fetch(`${url}${id}`);
+        const data = await response.json();
+        if(data.drinks){
+          const {
+          strDrink: name, 
+          strDrinkThumb: image,
+          strAlcoholic: info,
+          strCategory: category,
+          strGlass: glass,
+          strInstructions: instructions,
+          strIngredient1,
+          strIngredient2,
+          strIngredient3,
+          strIngredient4,
+          strIngredient5,
+         } = data.drinks[0]
+         const ingredients = [
+          strIngredient1,
+          strIngredient2,
+          strIngredient3,
+          strIngredient4,
+          strIngredient5,
+         ]
+         const newCocktail = {
+          name, 
+          image, 
+          info, 
+          category,
+          glass,
+          instructions,
+          ingredients,
+         }
+         setCocktail(newCocktail)
+        }else {
+          setCocktail(null)
+        }
+        setLoading(false)
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    }
+    getCocktail();
+  }, [id])
+  if(loading) {
+    return <Loading />
+  }
+  if (!cocktail) {
+    return <h2 className='section-title'>no cocktail to display</h2>
+  }
+  const {
+    name, 
+    image, 
+    info, 
+    category,
+    glass,
+    instructions,
+    ingredients,
+       } = cocktail;
   return (
-    <div>
-      <h2>single cocktail page </h2>
-    </div>
+    <section>
+      <h2 className='section-title'>{name}</h2>
+    </section>
   )
 }
 
