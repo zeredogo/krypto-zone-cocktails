a9e63c06654d15f4f20afab8a012c9957a0b3025
zeredogo <zeredogo@gmail.com>
2022-08-27 00:42:00 +0100

    updated commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,4 +27,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -35,4 +35,4 @@ const SearchForm = () => {
   )
 }
 
-export default SearchForm
+export default SearchForm;
