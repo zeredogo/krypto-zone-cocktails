a51163ddd6e8656b8bfd5ce5408d851114ce345a
zeredogo <zeredogo@gmail.com>
2022-08-27 00:59:27 +0100

    updated commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,15 +5,11 @@ const About = () => {
     <section className='section about-section'>
       <h1 className='section-title'>about us</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipissicing elit.
-        Eos doloribus
-        animi pariatur dignissimos nulla iure dolorum et fugit
-        minus hie.
-        Adipisci sint enim, sit quo impedit doloribus
-        accusantium, ipsum esse
-        distinctio accusamus laborum illum similique cupiditate
-        consequatur
-        quasi ab. Iure accusamus.
+      An open, crowd-sourced of 
+      drinks and cocktails from around the world. 
+      The site is just for you to know your
+      drinks and know the ingredients,
+      so you can keep your health clean.      
       </p>
     </section>
   )
